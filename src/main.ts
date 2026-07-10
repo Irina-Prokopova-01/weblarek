4ad83b0d7d442e08dcfeddfaaecaab94ddb74fec
Irina <Irina-prokopova.style@yandex.ru>
2026-07-10 14:05:26 -0700

    Check that data models Products are completely self-contained and operate independently of the rest of the application.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import './scss/styles.scss';
+import { apiProducts} from "./utils/data.ts";
+import { Products } from "./components/Models/Products.ts";
+
+const productsModel = new Products();
+productsModel.saveProducts(apiProducts.items);
+console.log(`Массив товаров из каталога:`, productsModel.getProducts())
 // import { Api } from './components/base/Api'
 // import { API_URL } from './utils/constants';
 //
