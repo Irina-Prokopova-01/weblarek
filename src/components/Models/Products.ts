94cfc71f8248d206f4cc89bd3dece551186b72fd
Irina <Irina-prokopova.style@yandex.ru>
2026-07-30 12:50:46 -0700

    Add events for method saveProducts

--- src/components/Models/Products.ts
+++ src/components/Models/Products.ts
@@ -1,16 +1,20 @@
 import { IProduct } from "../../types"
+import { IEvents } from "../base/Events.ts";
 
 export class Products {
     private products: IProduct[] = [];
     private selectProduct: IProduct | null = null;
 
+    constructor(private events: IEvents) {}
+    saveProducts(products: IProduct[]): void {
+        this.products = [...products];
+        this.events.emit("products:changed");
+    }
+
     getProducts(): IProduct [] {
         return [...this.products];
     }
 
-    saveProducts(products: IProduct[]): void {
-        this.products = [...products];
-    }
 
     getProductById(id:string): IProduct | undefined {
         return  this.products.find(product => product.id === id);
