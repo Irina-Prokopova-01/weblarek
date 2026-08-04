10e231faef00fd7e4e63a9e05cddad4db1bdb616
Irina <Irina-prokopova.style@yandex.ru>
2026-08-04 16:54:17 -0700

    Some correct

--- src/components/Views/OrderSuccess.ts
+++ src/components/Views/OrderSuccess.ts
@@ -3,21 +3,21 @@ import {Component} from "../base/Component.ts";
 import {ensureElement} from "../../utils/utils.ts";
 import {IEvents} from "../base/Events.ts";
 
-export class orderSuccess extends Component<ISuccess> {
-    protected successOrderChekc: HTMLElement;
+export class OrderSuccess extends Component<ISuccess> {
+    protected successOrderCheck: HTMLElement;
     protected successOrderButton: HTMLButtonElement;
     constructor(container: HTMLElement, protected events: IEvents) {
         super(container);
 
-        this.successOrderChekc = ensureElement<HTMLElement>('.order-success__description', this.container);
-        this.successOrderButton = ensureElement<HTMLButtonElement>('.button order-success__close', this.container);
+        this.successOrderCheck = ensureElement<HTMLElement>('.order-success__description', this.container);
+        this.successOrderButton = ensureElement<HTMLButtonElement>('.order-success__close', this.container);
 
         this.successOrderButton.addEventListener('click', () => {
             this.events.emit('success-modal:close');
 
         })
     }
-    set check(value: number) {
-        this.successOrderChekc.textContent = `Списано ${value} синапсов`;
+    set description(value: string) {
+        this.successOrderCheck.textContent = `Списано ${value} синапсов`;
     }
 }
