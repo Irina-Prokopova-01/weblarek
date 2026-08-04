977e0571262232c38cb3034e1c29575daa371894
Irina <Irina-prokopova.style@yandex.ru>
2026-08-03 20:17:39 -0700

    Add ContactForm.ts with class ContactForm

---
+++ src/components/Views/ContactForm.ts
@@ -0,0 +1,33 @@
+import {BaseForm} from "./BaseForm.ts";
+import { IContactFormActions} from "../../types";
+import {ensureElement} from "../../utils/utils.ts";
+
+
+export class ContactForm extends BaseForm {
+    protected emailInputContactForm: HTMLInputElement;
+    protected phoneInputContactForm: HTMLInputElement;
+
+    constructor(container: HTMLElement, actions?: IContactFormActions) {
+        super(container);
+        this.emailInputContactForm = ensureElement<HTMLInputElement>('[name="email"]', this.container);
+        this.phoneInputContactForm = ensureElement<HTMLInputElement>('[name="phone"]', this.container);
+
+        this.phoneInputContactForm.addEventListener('input', () => {
+            actions?.onPhone(this.phoneInputContactForm.value)
+        })
+
+        this.emailInputContactForm.addEventListener('input', () => {
+            actions?.onEmail(this.emailInputContactForm.value)
+
+        })
+    }
+
+    set email (value: string) {
+        this.emailInputContactForm.value = value;
+    }
+
+    set phone (value: string) {
+        this.phoneInputContactForm.value = value;
+    }
+
+}
