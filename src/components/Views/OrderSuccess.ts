50fb2d5bb410821d367face9670923fb7b8d817d
Irina <Irina-prokopova.style@yandex.ru>
2026-08-06 09:24:46 -0700

    Some correct

--- src/components/Views/OrderSuccess.ts
+++ src/components/Views/OrderSuccess.ts
@@ -1,23 +1,21 @@
-import { ISuccess } from "../../types";
+import { ISuccess, ISuccessActions } from "../../types";
 import {Component} from "../base/Component.ts";
 import {ensureElement} from "../../utils/utils.ts";
-import {IEvents} from "../base/Events.ts";
 
 export class OrderSuccess extends Component<ISuccess> {
     protected successOrderCheck: HTMLElement;
     protected successOrderButton: HTMLButtonElement;
-    constructor(container: HTMLElement, protected events: IEvents) {
+    constructor(container: HTMLElement, actions?: ISuccessActions) {
         super(container);
 
         this.successOrderCheck = ensureElement<HTMLElement>('.order-success__description', this.container);
         this.successOrderButton = ensureElement<HTMLButtonElement>('.order-success__close', this.container);
 
-        this.successOrderButton.addEventListener('click', () => {
-            this.events.emit('success-modal:close');
-
-        })
+        if (actions?.onClose) {
+            this.successOrderButton.addEventListener("click", actions.onClose);
+        }
     }
     set description(value: string) {
-        this.successOrderCheck.textContent = `Списано ${value} синапсов`;
+        this.successOrderCheck.textContent = value;
     }
 }
